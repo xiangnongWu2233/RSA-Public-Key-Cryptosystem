122a369e50118182d29a15ba4f2a43c456dbcd2b
Xiangnong Wu <xiangnongWu2233@icloud.com>
2022-02-21 21:23:39 -0500

    2-21-2022 21:23

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from encoder import generate_key,fast_power,encoding,primality_test,toCode
 from decoder import decoding,solve
-
+from tkinter import Tk,Button
 
 def main():
-    generate_key()
-    rule=open("rule.txt","r")
-    m=rule.readline()
-    k=rule.readline()
-    code=encoding()
+    root=Tk()
+    root.title('RSA Public Key Cryptosystem')
+
+    Button1=Button(root,text='Practice')
+    Button2=Button(root,text='Encryption')
+    Button3=Button(root,text='Attack')
+
+
+    Button1.pack()
+    Button2.pack()
+    Button3.pack()
+    root.mainloop()
+
+if __name__=="__main__":
+    main()
